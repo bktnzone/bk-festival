8827a03ce1c9301bdbdf53d16344758932716d58
Nirmal <Nirmal@nkdews.me>
2020-12-13 15:46:28 +0530

    Initial Commit
    Initial Commit

---
+++ pages/card.tsx
@@ -0,0 +1,77 @@
+import {useState} from 'react'
+import {useRouter} from 'next/router'
+
+const preventDefault = f => e => {
+    e.preventDefault()
+    f(e)
+}
+
+export default({
+    action = '/search'
+}) => {
+    const router = useRouter()
+    const [query, setQuery] = useState('')
+
+    const handleParam = setValue => e => setValue(e.target.value)
+
+    const handleSubmit = preventDefault(() => {
+        router.push({
+            pathname: action,
+            query: {
+                q: query
+            }
+        })
+    })
+
+    return (
+        <div className='md:flex bg-white rounded-lg p-6'>
+
+            <div className="w-full min-h-screen bg-gray-100">
+                <div className="max-w-2xl mx-auto flex flex-col items-center justify-center px-4">
+
+
+                    <div className="py-6 w-full">
+
+                        <div className="my-3 bg-white shadow rounded-lg">
+                            <div className="px-4 py-5 sm:p-6">
+                                <div className="flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 text-green-600">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+                                    </svg>
+                                    <h3 className=" px-2 text-lg leading-6 font-semibold text-gray-900">
+                                        Your Special Card is here
+                                    </h3>
+                                </div>
+
+                            </div>
+                        </div>
+                    
+                        <div className="my-3 bg-white shadow rounded-lg">
+                            <div className="px-4 py-5 sm:p-6">
+                                <div className="flex items-center">
+                                <img   width="888" height="630" src="https://lh3.googleusercontent.com/pw/ACtC-3e9WBWc0_PVCCktgDirRABP33ggn522nS2KwL_LgJ2_58p6s2fvkUhOImBYlKE4_mO-IM9PLGlTrbBmqYTe9YKOe-xcr_p0WgdetYK_mbRrdf-OZ4uE7PKyCeGS0KSLgwBuzjtJ_SMKIARuSA9USRJCVw=w888-h630-no?authuser=0" data-atf="false" data-iml="12638.099999996484"    />
+                                    
+                                </div>
+                              
+                           
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-2 max-w-xl text-xs font-medium leading-5 text-gray-500 overflow-hidden">
+                        <p>
+                            @Copyrights 2020. Brahma Kumaris - Youth Wing
+                       
+                        </p>
+                    </div>
+                    <div className="mt-1 text-sm leading-5 text-center">
+                        <a href="http://asdasd" target="_blank" className="text-center font-medium text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150">
+                            https://bkyouth.org
+                        </a>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,85 @@
+import {useState} from 'react'
+import {useRouter} from 'next/router'
+
+const preventDefault = f => e => {
+  e.preventDefault()
+  f(e)
+}
+
+export default function Home({action = '/card'}){
+   const router = useRouter()
+   const [query, setQuery] = useState('')
+
+   const handleParam = setValue => e => setValue(e.target.value)
+
+   const handleSubmit = preventDefault(() => {
+     router.push({
+       pathname: action,
+       query: {q: query},
+     })
+   })
+
+   return (
+
+    <div className='md:flex bg-white rounded-lg p-6'>
+
+    <div className="w-full min-h-screen bg-gray-100">
+        <div
+          className="max-w-2xl mx-auto flex flex-col items-center justify-center px-4"
+        >
+    
+  
+          <h1 className="text-blue-900 font-black text-5xl  text-center">
+            Special Card for You
+          </h1>
+          <h2 className="text-indigo-700 text-sm font-semibold italic">
+            Get your blessing by entering your name
+          </h2>
+    
+         
+          <div className=" w-full mt-6 flex rounded-md shadow-sm">
+            <div className="relative flex-grow focus-within:z-10">
+
+           
+
+            <form onSubmit={handleSubmit}>
+
+            <div className=" w-full mt-6 flex rounded-md shadow-sm">
+              <div className="relative flex-grow focus-within:z-10">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                 
+                </div> 
+                <input placeholder="Your name" value={query}
+                onChange={handleParam(setQuery)} className="text-gray-700 py-3 form-input block w-full rounded-none rounded-l-md pl-5 transition ease-in-out duration-150 font-semibold sm:text-sm sm:leading-5" />
+
+                </div>
+                <button className="group -ml-px relative inline-flex items-center px-3 py-3 border border-indigo-300 text-sm leading-5 font-medium rounded-r-md text-white bg-indigo-700 hover:text-indigo-700 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-indigo-300 active:bg-gray-100 active:text-indigo-700 transition ease-in-out duration-150">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-white h-5 w-5 group-hover:text-indigo-700"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg> <span className="ml-2 text-sm font-semibold"></span>
+                </button>
+                </div>
+
+              </form>
+            </div>
+    
+          </div>
+          <div className="mt-2 max-w-xl text-xs font-medium leading-5 text-gray-500 overflow-hidden">
+                        <p>
+                            @Copyrights 2020. Brahma Kumaris - Youth Wing
+                       
+                        </p>
+                    </div>
+                    <div className="mt-1 text-sm leading-5 text-center">
+                        <a href="http://asdasd" target="_blank" className="text-center font-medium text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150">
+                            https://bkyouth.org
+                        </a>
+                    </div>
+        </div>
+       
+      </div>
+         
+        </div>
+     
+   )
+}
+
+
